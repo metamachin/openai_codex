ca7a6cf975e5b8aa969412a2647ef8e825dae3f9
metamachin <metamachin@gmail.com>
2023-03-07 21:00:52 +0530

    onrender.com URL added

--- client/script.js
+++ client/script.js
@@ -85,8 +85,8 @@ const handleSubmit = async (e) => {
 
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
-    // Render fetch api https://codex-im0y.onrender.com/
-    const response = await fetch('http://localhost:5000/', {
+    // Render fetch api https://codex-im0y.onrender.com/ or http://localhost:5000/
+    const response = await fetch('https://codex-le3s.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
